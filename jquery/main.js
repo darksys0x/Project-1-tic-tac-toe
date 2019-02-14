3eb769f81d9d6089589e58dfb70796a0a1505b47
hamad-SHE <eng_cis1@hotmail.com>
2019-02-14 19:21:23 +0300

    adding winner

--- jquery/main.js
+++ jquery/main.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
 
-    var squer = $('.squer');
+      
 
     for(var i=0;i<9;i++){
-        $("#noumbersOfscouers").append("<div class=squer id=s" + i + " postion= id=s" + i + "></div>");
+        $("#noumbersOfscouers").append("<div class=squer id=s" + i + " data-postion=" + i + "></div>");
         
 
     }
@@ -12,15 +12,17 @@ $(document).ready(function(){
 
 
   $(".squer").on("click",you);
-  winner();
+//   winner();
 });
 
 
+
 function you(){
-        var isclass = $(this).attr("class");  //from 
+        var isclass = $(this).attr("class");   
         if(isclass.indexOf("assinged")< 0 ){
 
              $(this).addClass("eaxX assinged");
+             sideWiner($(this).data("postion"),"eaxX");
                   placRndom();
       
         }else{alert("alaredy selected");}
@@ -36,38 +38,214 @@ function placRndom(){
     console.log(rndmPostion);//tested
     var randomelemnts = select[rndmPostion]
     console.log(randomelemnts);
-    $(randomelemnts).addClass("os assinged");
+    $(randomelemnts).addClass("os assinged");//o mark
+    var curentPostion = $(randomelemnts).data("postion");
+
+    sideWiner(curentPostion,"os");
+
     
+
+
+}
+// 0 1 2
+// 3 4 5
+// 6 7 8
+
+// function sideWiner(pos, mark){
+//             var w = [[0, 1, 2],[3, 4, 5],[6, 7, 8],[0, 3, 6],[1, 4, 7],[2, 5, 8],[0, 4, 8],[2, 4, 6]];
+//             $.each(winpostion,function(key,arr){
+//                 if(arr.indexOf(pos)>=0){
+//                     console.log(mark);
+//                     console.log(arr);
+//         }
+//     })
+// }
+    
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function sideWiner(pos,mark){  
+    //digram form 
+        // 0-1-2
+        // 3-4-5
+        // 6-7-8
+    var winpostion = [
+        // horzitenal
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+      
+        //verctioal
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+      
+        // dignoures
+        [0, 4, 8],
+        [2, 4, 6]
+      ];
+      
+    if($(".eaxX").length >= 3 || $(".os").length >= 3){
+
+        $.each(winpostion, function(key, arr){
+                if(arr.indexOf(pos) >=0){
+                    console.log(arr);
+                    var marksInArro = 0;
+                    $.each(arr,function(index,value){
+                        var className = $("#s"+value).attr("class");
+                        if(className.indexOf(mark) >= 0){
+                            marksInArro++;
+                            if(marksInArro == 3){
+                                if(mark == "eaxX"){
+                                   displayRe("you win")
+                                }else if(mark == "os"){
+                                    displayRe("you lost")
+
+                                }
+                            }
+                        }
+
+
+                    })
+                }
+
+            })
+
+    }
+    
+
 
 
 }
 
 
 
-var counterPlayer =0; //this from help me abdraboua 
-function winner (){
 
 
-    var winLtrack=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[2,4,7],[3,5,8],[2,4,6],[0,4,8]]; 
-     //$(eaxX).length >= 0 || $("o").length>=0;
-      for(var i=0;i<=winLtrack.length;i++){
+
+
+
+function displayRe(ma){
+    $("#r").html(ma);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var counterPlayer =0; //this from help me abdraboua 
+// function winner (){
+
+
+//     var winLtrack=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[2,4,7],[3,5,8],[2,4,6],[0,4,8]]; 
+//      //$(eaxX).length >= 0 || $("o").length>=0;
+//       for(var i=0;i<=winLtrack.length;i++){
 
         
-        var a = winLtrack[i][0];
-        var b = winLtrack[i][1];
-        var c = winLtrack[i][2];
+//         var a = winLtrack[i][0];
+//         var b = winLtrack[i][1];
+//         var c = winLtrack[i][2];
 
-        console.log( 'a' , a , "b" , b ,'c' , c);
+//         console.log( 'a' , a , "b" , b ,'c' , c);
 
-        console.log($('.squer')[a]);
-        if ( $([a]) !== '' && $('.squer'[a]) == $('.squer'[b])&& $('.squer'[b]) == $('.squer'[c]) ) {
-            alert('player '+ $('.squer'[a]).html() +'is winner');
-        }
-    }
-     if ( counterPlayer == 8){ // if any one not winer
-        alert('No one is winner');
-    } 
-};
+//         console.log($('.squer')[a]);
+//         if ( $([a]) !== '' && $('.squer'[a]) == $('.squer'[b])&& $('.squer'[b]) == $('.squer'[c]) ) {
+//             alert('player '+ $('.squer'[a]).html() +'is winner');
+//         }
+//     }
+//      if ( counterPlayer == 8){ // if any one not winer
+//         alert('No one is winner');
+//     } 
+// };
 
 // var grid = document.querySelector("#gird");
 // function creatDiv(){
@@ -86,9 +264,3 @@ function winner (){
     // }
     
     // document.appendChild(toAdd);
-
-
-    
-
-
-    //---------------------
